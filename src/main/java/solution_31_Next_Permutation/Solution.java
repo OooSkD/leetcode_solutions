2fb930de8af630e863ef8fdb1b150b97484a30e9
OooSkD <OooSkD@users.noreply.github.com>
2025-05-02 00:22:55 +0300

    added task

---
+++ src/main/java/solution_31_Next_Permutation/Solution.java
@@ -0,0 +1,34 @@
+package solution_31_Next_Permutation;
+
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        int j = nums.length - 1;
+
+        if (i >= 0) {
+            while (nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
