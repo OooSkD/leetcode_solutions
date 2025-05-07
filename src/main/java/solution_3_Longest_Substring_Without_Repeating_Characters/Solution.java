65a98632e1823ef7a6967747db3f1fdca67554b6
OooSkD <OooSkD@users.noreply.github.com>
2025-05-07 16:07:59 +0300

    added task

---
+++ src/main/java/solution_3_Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -0,0 +1,21 @@
+package solution_3_Longest_Substring_Without_Repeating_Characters;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> charSet = new HashSet<>();
+        int left = 0;
+        int maxLength = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while (charSet.contains(s.charAt(right))) {
+                charSet.remove(s.charAt(left));
+                left++;
+            }
+            charSet.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
